ee8d2c82db4da90494af9b7e0ce70c6b51834971
Xuyan Lu <lluxuyan2015@outlook.com>
2022-11-17 21:48:18 -0600

    add goerli deployment setting

--- smart-contract/truffle-config.js
+++ smart-contract/truffle-config.js
@@ -1,39 +1,9 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation, and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * https://trufflesuite.com/docs/truffle/reference/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
+const dotenv = require("dotenv");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+dotenv.config();
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
     development: {
       host: "127.0.0.1",
@@ -45,38 +15,23 @@ module.exports = {
       port: 7545,
       network_id: "*",
     },
+    goerli: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.APP_PRIVATE_KEY,
+          process.env.APP_GOERLI_RPC_URL
+        ),
+      network_id: 5,
+      from: "0x1e4BA1401de4D74f7D4F6e383E848baa881B5277",
+    },
   },
-
-  // Set default mocha options here, use special reporters, etc.
-  mocha: {
-    // timeout: 100000
+  etherscan: {
+    apiKey: process.env.APP_ETHERSCAN_API_KEY,
   },
-
   // Configure your compilers
   compilers: {
     solc: {
       version: "0.8.14",
     },
   },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows:
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-  //   enabled: false,
-  //   host: "127.0.0.1",
-  //   adapter: {
-  //     name: "sqlite",
-  //     settings: {
-  //       directory: ".db"
-  //     }
-  //   }
-  // }
 };
